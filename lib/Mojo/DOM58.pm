a9b673cd450ae482584288fd2b7e502e0b8bcad5
Dan Book <grinnz@grinnz.com>
2021-04-04 23:29:37 -0400

    we are now up to 9.0

--- lib/Mojo/DOM58.pm
+++ lib/Mojo/DOM58.pm
@@ -431,7 +431,7 @@ closely compatible with upstream. It differs only in the standalone format and
 compatibility with Perl 5.8. Any bugs or patches not related to these changes
 should be reported directly to the L<Mojolicious> issue tracker.
 
-This release of L<Mojo::DOM58> is up to date with version C<8.09> of
+This release of L<Mojo::DOM58> is up to date with version C<9.0> of
 L<Mojolicious>.
 
 =head1 NODES AND ELEMENTS
